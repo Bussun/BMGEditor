b4970add7cf98208bf086a22d5839dc742c98e7f
Denis <Denis@DESKTOP-VVK92VA>
2021-11-09 08:52:14 +0200

    Add project files.

---
+++ BMGEditor/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace BMGEditor
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            
+        }
+    }
+}
