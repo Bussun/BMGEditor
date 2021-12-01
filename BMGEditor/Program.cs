1d6c0199c62fd5a6f7d107efc1a54a67f68b25cf
Denis <denis.leduc@protonmail.com>
2021-12-01 16:46:59 +0100

    Additions and WIP Deleted obsolete code New additions: base for an updater WIP: Making the editor read FLW1 and FLI1 from provided file

--- BMGEditor/Program.cs
+++ BMGEditor/Program.cs
@@ -4,13 +4,16 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Text;
 using System.Globalization;
+using System.Net.Http;
+using System.IO;
 
 namespace BMGEditor
 {
     public static class Variables
     {
         public const string softwareName = "Luma";
-        public const string softwareVersion = "v0.4";
+        public const string softwareVersion = "v0.4.5";
+        public const UInt64 softwareInternalVersion = 45;
         public const bool isBeta = true;
         public const bool isPrivateBeta = false;
     }
@@ -23,15 +26,40 @@ namespace BMGEditor
             Bcsv.PopulateHashtable();
             if (Variables.isBeta && Variables.isPrivateBeta) MessageBox.Show("This is a private beta, please don\'t leak it.", "Private", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             Application.Run(new MainForm());
-
-            //Test();
         }
 
-        static void Test()
+        static readonly HttpClient wrClient = new HttpClient();
+        static async void CheckUpdates()
         {
-            string str = "FF";
-            Byte octet = Byte.Parse(str, NumberStyles.HexNumber);
-            Console.WriteLine(octet);
+            const string verCheckURL = "https://bussun.github.io/res/checks/luma/upre1";
+            Stream wrAnswer;
+
+            try
+            {
+                wrAnswer = await wrClient.GetStreamAsync(verCheckURL);
+                StreamReader wrAnswerReader = new StreamReader(wrAnswer);
+                string wrAnswerContent = wrAnswerReader.ReadToEnd();
+                
+                if (wrAnswerContent.Length != 0)
+                {
+                    UInt64 wrAnswerVersion = UInt64.Parse(wrAnswerContent);
+
+                    if (wrAnswerVersion > Variables.softwareInternalVersion)
+                        MessageBox.Show("New version available", "Update available", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    else if (wrAnswerVersion == Variables.softwareInternalVersion)
+                        MessageBox.Show("Luma is up to date.", "No update available", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    else
+                        MessageBox.Show("Woah, you got a developpement version !", "The most up to date ever (for now)", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Couldn\'t check for updates, null response", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Couldn\'t check for updates", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
